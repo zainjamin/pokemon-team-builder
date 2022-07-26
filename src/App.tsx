39f36420e3e3cd59b9ce4cb7dfe87fa5f434c7d7
Zain Salman <zainsalman04@gmail.com>
2022-07-26 01:34:46 -0400

    started PokeSelector

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
 import './App.css';
+import { PokeSelect } from './components/PokeSelect';
 
 const App: FC = () => {
   return (
     <div>
-      
+      <PokeSelect game="2" />
     </div>
   )
 };
---
+++ src/components/PokeSelect.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react';
+import { getMultiPokdex, getPokedex } from '../PokeApi';
+import AsyncSelect from 'react-select/async';
+
+interface PokeSelectProps {
+  game: string;
+}
+
+const getPokemon = async (game: string) => {
+  const pokemon = game.includes('+')
+    ? await getMultiPokdex(game)
+    : await getPokedex(game);
+  const pokemonValues = [].concat.apply([], pokemon).map((name: string) => {
+    return { value: name, label: name.toUpperCase() };
+  });
+  return pokemonValues;
+};
+
+export const PokeSelect: FC<PokeSelectProps> = (props) => {
+  const { game } = props;
+  const filterPokemon = async (inputValue: string) => {
+    const pokemonList = await getPokemon(game);
+    return pokemonList.filter((pkmn) => {
+      return pkmn.label.includes(inputValue.toUpperCase());
+    })
+  };
+  return (
+    <AsyncSelect
+      cacheOptions
+      defaultOptions
+      loadOptions={filterPokemon}
+    />
+  );
+};
